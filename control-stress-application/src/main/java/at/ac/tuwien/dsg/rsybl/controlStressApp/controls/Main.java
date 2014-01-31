7f79be53bb55276af1085c6aec088268cec901d6
Georgiana Copil <e.copil@dsg.tuwien.ac.at>
2014-01-31 14:31:37 +0100

    modified stress calls

--- control-stress-application/src/main/java/at/ac/tuwien/dsg/rsybl/controlStressApp/controls/Main.java
+++ control-stress-application/src/main/java/at/ac/tuwien/dsg/rsybl/controlStressApp/controls/Main.java
@@ -22,42 +22,42 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		FlexiantActions flexiantActions = new FlexiantActions();
-		System.out.println(flexiantActions.createNewServer("DataNode_new",
-				"c569fa37-a8fa-384a-8ab1-365efb157105", 2, 4));
-//		InputProcessing inputProcessing = new InputProcessing();
-//		JAXBContext jc;
-//		CloudServiceXML applicationDescription = null;
-//		DeploymentDescription deploymentDescription = null;
-//		try {
-//			jc = JAXBContext.newInstance(CloudServiceXML.class);
-//		
-//		Unmarshaller u = jc.createUnmarshaller();
-//
-//		 applicationDescription = (CloudServiceXML)  u.unmarshal(new File(Configuration
-//				.getApplicationDescription()));
-//		//applicationDescription = element.getValue();
-//
-//		
-//
-//		JAXBContext jc1 = JAXBContext.newInstance(DeploymentDescription.class);
-//		Unmarshaller u1 = jc1.createUnmarshaller();
-//
-//		deploymentDescription = (DeploymentDescription) u1.unmarshal(new File(Configuration
-//				.getDeploymentDescription()));
-//
-//		} catch (JAXBException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//
-//		DependencyGraph dependencyGraph = inputProcessing.loadDependencyGraphFromObjects(applicationDescription, new SYBLElasticityRequirementsDescription(),
-//						deploymentDescription);
-//		System.out.println(dependencyGraph.graphToString());
-//	
-//		RandomControlGeneration controlGeneration = new RandomControlGeneration(
-//				dependencyGraph);
+//		FlexiantActions flexiantActions = new FlexiantActions();
+//		System.out.println(flexiantActions.createNewServer("DataNode_new",
+//				"c569fa37-a8fa-384a-8ab1-365efb157105", 2, 4));
+		InputProcessing inputProcessing = new InputProcessing();
+		JAXBContext jc;
+		CloudServiceXML applicationDescription = null;
+		DeploymentDescription deploymentDescription = null;
+		try {
+			jc = JAXBContext.newInstance(CloudServiceXML.class);
+		
+		Unmarshaller u = jc.createUnmarshaller();
+
+		 applicationDescription = (CloudServiceXML)  u.unmarshal(new File(Configuration
+				.getApplicationDescription()));
+		//applicationDescription = element.getValue();
+
+		
+
+		JAXBContext jc1 = JAXBContext.newInstance(DeploymentDescription.class);
+		Unmarshaller u1 = jc1.createUnmarshaller();
+
+		deploymentDescription = (DeploymentDescription) u1.unmarshal(new File(Configuration
+				.getDeploymentDescription()));
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		DependencyGraph dependencyGraph = inputProcessing.loadDependencyGraphFromObjects(applicationDescription, new SYBLElasticityRequirementsDescription(),
+						deploymentDescription);
+		System.out.println(dependencyGraph.graphToString());
+	
+		RandomControlGeneration controlGeneration = new RandomControlGeneration(
+				dependencyGraph);
 
 	}
 
