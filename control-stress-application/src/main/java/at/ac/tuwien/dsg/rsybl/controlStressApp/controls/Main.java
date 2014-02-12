4697e863026c338c2c1b489405f4065ea96f23f3
Georgiana Copil <e.copil@dsg.tuwien.ac.at>
2014-02-12 16:49:29 +0100

    after nic cleaned

--- control-stress-application/src/main/java/at/ac/tuwien/dsg/rsybl/controlStressApp/controls/Main.java
+++ control-stress-application/src/main/java/at/ac/tuwien/dsg/rsybl/controlStressApp/controls/Main.java
@@ -23,44 +23,44 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		FlexiantActions flexiantActions = new FlexiantActions();
-		flexiantActions.cleanNics();
+//		FlexiantActions flexiantActions = new FlexiantActions();
+//		flexiantActions.cleanNics();
 //		System.out.println(flexiantActions.createNewServer("DataNode_new",
 //				"c7ee301c-813c-34f4-827a-783f22312dcb", 2, 4));
 //		flexiantActions.listAllNics();
-//		InputProcessing inputProcessing = new InputProcessing();
-//		JAXBContext jc;
-//		CloudServiceXML applicationDescription = null;
-//		DeploymentDescription deploymentDescription = null;
-//		try {
-//			jc = JAXBContext.newInstance(CloudServiceXML.class);
-//		
-//		Unmarshaller u = jc.createUnmarshaller();
-//
-//		 applicationDescription = (CloudServiceXML)  u.unmarshal(new File(Configuration
-//				.getApplicationDescription()));
-//		//applicationDescription = element.getValue();
-//
-//		
-//
-//		JAXBContext jc1 = JAXBContext.newInstance(DeploymentDescription.class);
-//		Unmarshaller u1 = jc1.createUnmarshaller();
-//
-//		deploymentDescription = (DeploymentDescription) u1.unmarshal(new File(Configuration
-//				.getDeploymentDescription()));
-//
-//		} catch (JAXBException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//
-//		DependencyGraph dependencyGraph = inputProcessing.loadDependencyGraphFromObjects(applicationDescription, new SYBLElasticityRequirementsDescription(),
-//						deploymentDescription);
-//		System.out.println(dependencyGraph.graphToString());
-//	
-//		RandomControlGeneration controlGeneration = new RandomControlGeneration(
-//				dependencyGraph);
+		InputProcessing inputProcessing = new InputProcessing();
+		JAXBContext jc;
+		CloudServiceXML applicationDescription = null;
+		DeploymentDescription deploymentDescription = null;
+		try {
+			jc = JAXBContext.newInstance(CloudServiceXML.class);
+		
+		Unmarshaller u = jc.createUnmarshaller();
+
+		 applicationDescription = (CloudServiceXML)  u.unmarshal(new File(Configuration
+				.getApplicationDescription()));
+		//applicationDescription = element.getValue();
+
+		
+
+		JAXBContext jc1 = JAXBContext.newInstance(DeploymentDescription.class);
+		Unmarshaller u1 = jc1.createUnmarshaller();
+
+		deploymentDescription = (DeploymentDescription) u1.unmarshal(new File(Configuration
+				.getDeploymentDescription()));
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		DependencyGraph dependencyGraph = inputProcessing.loadDependencyGraphFromObjects(applicationDescription, new SYBLElasticityRequirementsDescription(),
+						deploymentDescription);
+		System.out.println(dependencyGraph.graphToString());
+	
+		RandomControlGeneration controlGeneration = new RandomControlGeneration(
+				dependencyGraph);
 		
 	}
 
