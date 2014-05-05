c6879706e0d22ec5cda63d1bcb93a0d80c2cc87e
Georgiana Copil <e.copil@dsg.tuwien.ac.at>
2014-05-05 15:58:34 +0200

    control-stress-app deleted

--- control-stress-application/src/main/java/at/ac/tuwien/dsg/rsybl/controlStressApp/controls/ActionOnIaaSProvider.java
+++
@@ -1,7 +0,0 @@
-package at.ac.tuwien.dsg.rsybl.controlStressApp.controls;
-
-
-
-public abstract class ActionOnIaaSProvider {
-	public abstract String createNewServer(String serverName,String imageUUID, int cpu, int mem);
-}
