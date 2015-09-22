ecc72e3b9d7ece14794f9ea28987a5f1e1509e14
Georgiana Copil <e.copil@dsg.tuwien.ac.at>
2015-09-22 12:25:18 +0200

    sybl client new version

--- rSYBL-control-service-pom/rSYBL-client/src/main/java/at/ac/tuwien/dsg/rSybl/client/SYBLControlClient.java
+++ rSYBL-control-service-pom/rSYBL-client/src/main/java/at/ac/tuwien/dsg/rSybl/client/SYBLControlClient.java
@@ -23,109 +23,151 @@ package at.ac.tuwien.dsg.rSybl.client;
  */
 
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.xml.bind.JAXBContext;
+import javax.ws.rs.core.MediaType;
 
 public class SYBLControlClient {
 
-    protected String REST_API_URL = "http://localhost/rSYBL-analysis-engine-0.1-SNAPSHOT/restWS";
+    protected static String REST_API_URL = "http://localhost:8280/rSYBL/restWS";
     protected String compRules;
 
     public SYBLControlClient(String rsyblurl) {
         REST_API_URL = rsyblurl;
     }
-    
-    public void modifyAppDescription(String applicationID, String newAppDescription, String appDeployment, String effects) {
-        stopApplication(applicationID);
-        initialInstantiationLifecycle(applicationID,newAppDescription, appDeployment, effects, compRules);
-    }
-    
-    public void initialInstantiationLifecycle(String id,String appDescription, String appDeployment, String effects, String compRules) {
-        this.compRules = compRules;
-        setApplicationDescription(id,appDescription);
-        setApplicationDeployment(id,appDeployment);
-        setElasticityCapabilitiesEffects(id,effects);
-        setMetricsCompositionRules(id,compRules);
-    }
 
-    public void setApplicationDescription(String id,String appDescription) {
-        
-        callPUT(appDescription, id+"/description");        
-        
-        
-        
-    }
-    
-    public void setApplicationDeployment(String id,String appDescription) {
-        callPUT(appDescription, id+"/deployment");        
-        
-    }
-    
-    public void setMetricsCompositionRules(String id,String rules) {
-        
-        
-        callPUT(rules, id+"/metricsCompositionRules");
-        
-        
-    }
-
-    private void callPUT(String body, String methodName) {
-        
-        URL url = null;
-        HttpURLConnection connection = null;
+    public static void main(String[] args) {
         try {
-            url = new URL(REST_API_URL + "/" + methodName);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setInstanceFollowRedirects(false);
-            connection.setRequestMethod("PUT");
-            connection.setRequestProperty("Content-Type", "application/xml");
-            connection.setRequestProperty("Accept", "application/json");
+            String applicationID = "DataPlay";
+            String tosca = readFile("DataPlay.tosca", Charset.defaultCharset());
+            String deployment = readFile("deployment_DataPlay.xml", Charset.defaultCharset());
+            SYBLControlClient sYBLControlClient = new SYBLControlClient(REST_API_URL);
+            sYBLControlClient.prepareControl(applicationID);
+            sYBLControlClient.setApplicationDescription(applicationID, tosca);
+            sYBLControlClient.setApplicationDeployment(applicationID, deployment);
+//            sYBLControlClient.startTest(applicationID);
+//            sYBLControlClient.testElasticityCapability(applicationID, "Master", "attachDisk");
+//              sYBLControlClient.testElasticityCapability(applicationID, "Master", "dettachDisk");
+            sYBLControlClient.prepareControl(applicationID);
+            sYBLControlClient.setApplicationDescription(applicationID, tosca);
+            sYBLControlClient.setApplicationDeployment(applicationID, deployment);
+           sYBLControlClient.startApplication(applicationID);
 
-            //write message body
-            OutputStream os = connection.getOutputStream();
-            os.write(body.getBytes(Charset.forName("UTF-8")));
-            os.flush();
-            os.close();
-            
-            InputStream errorStream = connection.getErrorStream();
-            if (errorStream != null) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(errorStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    Logger.getLogger(SYBLControlClient.class.getName()).log(Level.SEVERE, line);
-                }
-            }
-            
-            InputStream inputStream = connection.getInputStream();
-            if (inputStream != null) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    Logger.getLogger(SYBLControlClient.class.getName()).log(Level.SEVERE, line);
-                }
-            }
-            
-        } catch (Exception e) {
-            Logger.getLogger(SYBLControlClient.class.getName()).log(Level.SEVERE, e.getMessage(), e);
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
+        } catch (IOException ex) {
+            Logger.getLogger(SYBLControlClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    private void callPOST(String body, String methodName) {
+    static String readFile(String path, Charset encoding)
+            throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+    public void modifyAppDescription(String applicationID, String newAppDescription, String appDeployment, String effects) {
+        stopApplication(applicationID);
+        initialInstantiationLifecycle(applicationID, newAppDescription, appDeployment, effects, compRules);
+    }
+
+    public void initialInstantiationLifecycle(String applicationID, String appDescription, String appDeployment, String effects, String compRules) {
+        this.compRules = compRules;
+        prepareControl(applicationID);
+        setApplicationDescription(applicationID, appDescription);
+        setApplicationDeployment(applicationID, appDeployment);
+        setElasticityCapabilitiesEffects(applicationID, effects);
+        setMetricsCompositionRules(applicationID, compRules);
+        startApplication(applicationID);
+    }
+
+    public void setApplicationDescription(String applicationID, String appDescription) {
+
+        callPUT(appDescription, applicationID + "/description/tosca");
+
+
+
+    }
+
+    public void testElasticityCapability(String applicationID, String componentID, String elasticityCapability) {
+
+
+        callPUT("", applicationID + "/" + componentID + "/testElasticityCapability/" + elasticityCapability);
+
+
+
+    }
+
+    public void startTest(String applicationID) {
+
+
+        callPUT("", applicationID + "/startTEST");
+
+
+
+    }
+
+    public void testElasticityCapabilityWithPlugin(String applicationID, String componentID, String pluginID, String elasticityCapability) {
+
+
+        callPUT("", applicationID + "/" + componentID + "/testElasticityCapability/" + pluginID + "/" + elasticityCapability);
+
+
+
+    }
+
+    public void setApplicationDeployment(String applicationID, String appDescription) {
+        callPUT(appDescription, applicationID + "/deployment");
+
+    }
+
+    public void prepareControl(String id) {
+
+
+        callPUT(id, id + "/prepareControl");
+
+
+    }
+
+    public void setMetricsCompositionRules(String id, String rules) {
+
+
+        callPUT(rules, id + "/metricsCompositionRules");
+
+
+    }
+
+    private void callPUT(String body, String methodName) {
+
+        Client c = Client.create();
+
+        c.getProperties().put(
+                ClientConfig.PROPERTY_FOLLOW_REDIRECTS, true);
+        WebResource r = c.resource(REST_API_URL + "/" + methodName);
         
+         r.accept(
+                MediaType.APPLICATION_XML_TYPE).
+                header("Content-Type", "application/xml; charset=utf-8").
+                header("Accept", "application/xml, multipart/related").
+                put(String.class, body);
+        
+        
+    }
+
+    private void callPOST(String body, String methodName) {
+
         URL url = null;
         HttpURLConnection connection = null;
         try {
@@ -142,7 +184,7 @@ public class SYBLControlClient {
             os.write(body.getBytes(Charset.forName("UTF-8")));
             os.flush();
             os.close();
-            
+
             InputStream errorStream = connection.getErrorStream();
             if (errorStream != null) {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(errorStream));
@@ -151,7 +193,7 @@ public class SYBLControlClient {
                     Logger.getLogger(SYBLControlClient.class.getName()).log(Level.SEVERE, line);
                 }
             }
-            
+
             InputStream inputStream = connection.getInputStream();
             if (inputStream != null) {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -160,7 +202,7 @@ public class SYBLControlClient {
                     Logger.getLogger(SYBLControlClient.class.getName()).log(Level.SEVERE, line);
                 }
             }
-            
+
         } catch (Exception e) {
             Logger.getLogger(SYBLControlClient.class.getName()).log(Level.SEVERE, e.getMessage(), e);
         } finally {
@@ -169,19 +211,19 @@ public class SYBLControlClient {
             }
         }
     }
-    
+
     public void startApplication(String applicationID) {
-        callPUT(applicationID, applicationID+"/startControl");
+        callPUT("", applicationID + "/startControl");
     }
 
     public void stopApplication(String applicationID) {
-        callPUT(applicationID, applicationID+"/stopControl");
+        callPUT("", applicationID + "/stopControl");
     }
 
-    public void setElasticityCapabilitiesEffects(String id,String effects) {
-        
-        callPUT(effects, id+"/elasticityCapabilitiesEffects");
-        
-        
+    public void setElasticityCapabilitiesEffects(String id, String effects) {
+
+        callPUT(effects, id + "/elasticityCapabilitiesEffects");
+
+
     }
 }
